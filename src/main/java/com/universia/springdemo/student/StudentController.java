862096d70f62d82cf3ba285a24ebc39b96e18aa2
alielmouedden <alielmouedden9@gmail.com>
2025-02-11 12:49:08 +0100

    Implementing service layer

--- src/main/java/com/universia/springdemo/student/StudentController.java
+++ src/main/java/com/universia/springdemo/student/StudentController.java
@@ -1,11 +1,11 @@
 package com.universia.springdemo.student;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
+
 import java.util.List;
 
 @RestController
